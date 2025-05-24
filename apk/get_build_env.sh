d0b2103ba81dff6b9d1ff870b49af2a3f24911a9
Sergey Safarov <s.safarov@anycast-lb.net>
2025-05-24 22:51:51 +0300

    aaaaa

--- apk/get_build_env.sh
+++ apk/get_build_env.sh
@@ -13,9 +13,9 @@ prepare_build_user() {
 	adduser -D build
 	addgroup build abuild
 	echo "%abuild ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/abuild
-	su - build -c "git config --global user.name 'Kamailio GitHub service user'"
-	su - build -c "git config --global user.email 'github@kamailio.org'"
-	su - build -c "abuild-keygen -a -i -q -n"
+	sudo -inu build git config --global user.name 'Kamailio GitHub service user'
+	sudo -inu build git config --global user.email 'github@kamailio.org'
+	sudo -inu build abuild-keygen -a -i -q -n
 }
 
 install_build_deps() {
