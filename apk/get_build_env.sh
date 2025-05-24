5b7f317268e074f5032c01c6f1be34e1c07605b5
Sergey Safarov <s.safarov@anycast-lb.net>
2025-05-24 21:30:08 +0300

    added Alpine linux builder scripts

---
+++ apk/get_build_env.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+OS_FILELIST=/tmp/os_filelist
+set -x
+set -o errexit -o nounset -o pipefail
+
+image_filelist() {
+	find / '(' -path /etc -o -path /dev -o -path /home -o -path /media -o -path /proc -o -path /mnt -o -path /root -o -path /sys -o -path /tmp -o -path /run ')' -prune -o -print
+}
+
+package_dumpcap() {
+	apkArch="$(apk --print-arch)"
+
+	case "${apkArch}" in
+	armhf)
+		echo "armhf arch does not have  wireshark-common package, skiping"
+		;;
+	*)
+		apk --no-cache add wireshark-common
+		addgroup build wireshark
+		;;
+	esac
+}
+
+install_common_deps() {
+	apk --no-cache add build-base git doas abuild tcpdump coreutils
+}
+
+prepare_build_user() {
+	adduser -D build
+	addgroup build abuild
+	echo "permit nopass :abuild as root" > /etc/doas.d/doas.conf
+	su - build -c "git config --global user.name 'Kamailio GitHub service user'"
+	su - build -c "git config --global user.email 'github@kamailio.org'"
+	su - build -c "abuild-keygen -a -i -n"
+}
+
+install_build_deps() {
+	cd /tmp
+	wget https://raw.githubusercontent.com/kamailio/kamailio/refs/heads/master/pkg/kamailio/alpine/APKBUILD
+	abuild -F deps
+	rm -f APKBUILD
+}
+
+apk --no-cache upgrade
+image_filelist >> ${OS_FILELIST}
+install_common_deps
+prepare_build_user
+install_build_deps
+package_dumpcap
